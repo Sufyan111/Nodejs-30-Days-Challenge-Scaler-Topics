5b3d5d7f0b371893fb0b798f3ff7ca187c16ab39
Syed Sufyan <syedaleem1742@gmail.com>
2024-02-01 12:01:03 +0530

    Day1 Done

---
+++ Day1/day1.js
@@ -0,0 +1,19 @@
+const fs = require("fs").promises;
+
+async function readFileContent(filePath) {
+  try {
+    const content = await fs.readFile(filePath, "utf8");
+    if(content === ""){
+        console.log(`""`)
+        return;
+    }
+    console.log(`File Content:\n${content}`);
+  } catch (error) {
+    console.error(`Error reading file: ${error.message}`);
+  }
+}
+
+// Test Cases
+// readFileContent("test-files/file1.txt");
+readFileContent("test-files/empty-file.txt");
+// readFileContent("test-files/nonexistent-file.txt");
